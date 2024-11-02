f50aa8ff76586ba138028045bb62d962cb5ad0c2
MrUtcka <72068465+MrUtcka@users.noreply.github.com>
2024-11-02 23:35:33 +0800

    Обновление основного скрипта

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-import keyboard
 
-def summarize_text(text, compression):
+def summarize_text_nltk(text, compression):
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(text)
+
+    freqTable = dict()
+    for word in words:
+        word = word.lower()
+        if word in stopWords:
+            continue
+        if word in freqTable:
+            freqTable[word] += 1
+        else:
+            freqTable[word] = 1
+
+    sentences = sent_tokenize(text)
+    sentenceValue = dict()
+
+    for sentence in sentences:
+        for word, freq in freqTable.items():
+            if word in sentence.lower():
+                if word in sentence.lower():
+                    if sentence in sentenceValue:
+                        sentenceValue[sentence] += freq
+                    else:
+                        sentenceValue[sentence] = freq
+
+                        sumValues = 0
+
+    for sentence in sentenceValue:
+        sumValues += sentenceValue[sentence]
+
+    average = int(sumValues / len(sentenceValue))
+    summary = ''
+
+    for sentence in sentences:
+        if (sentence in sentenceValue) and (sentenceValue[sentence] > (compression * average)):
+            summary += ' ' + sentence
+
+    return summary
+
+
+def summarize_text_sumy(text, compression):
     parser = PlaintextParser.from_string(text, Tokenizer("russian"))
     summarizer_lex = LexRankSummarizer()
 
@@ -14,22 +57,33 @@ def summarize_text(text, compression):
 
     return lex_summary
 
+
 def main():
     print('------| Сжиматель текста |------')
 
     while True:
-
         text_to_summ = input('Введите текст для сжатия: ')
         compression_force = input('Введите силу сжатия(0 - Слабое, 1 - Сильное): ')
-        
+
         print('-' * 50)
+
         if(compression_force == '0'):
-            print(summarize_text(text_to_summ, text_to_summ.count('.') // 2 + 1))
+            summ = summarize_text_nltk(text_to_summ, 1.2)
+            if(len(summ) == 0):
+                print(summarize_text_sumy(text_to_summ, text_to_summ.count('.') // 2 + 1))
+            else:
+                print(summ)
         elif(compression_force == '1'):
-            print(summarize_text(text_to_summ, 3))
+            summ = summarize_text_nltk(text_to_summ, 1.6)
+            if(len(summ) == 0):
+                print(summarize_text_sumy(text_to_summ, 3))
+            else:
+                print(summ)
         else:
             print('Неверные данные, повторите еще раз.')
+
         print('-' * 50)
+
 
 if __name__ == '__main__':
     main()
